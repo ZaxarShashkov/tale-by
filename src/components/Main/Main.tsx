492904b73194c7d3baaa3b9bca812e206dbf6266
ZaxarShashkov <zekharjah@gmail.com>
2024-02-16 22:51:38 +0300

    add main layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import styles from './Main.module.scss';
 import { TypeOfTest } from '../TypeOfTest/TypeOfTest';
 
+const tests = [
+	{ id: 1, title: 'Электрофизические измерения (ЭФИ)' },
+	{ id: 2, title: 'Высоковольтные испытания' },
+	{ id: 3, title: 'Наладка электрооборудования промышленных предприятий' },
+	{ id: 4, title: 'Специальные виды работ' },
+];
+
 export const Main = () => {
 	return (
 		<main className={styles.main}>
 			<h2 className={styles.main__title}>
 				Виды <span className={styles.main__title_hl}>Испытаний</span>
 			</h2>
-			<TypeOfTest />
-			<div className={styles.line}></div>
-			<TypeOfTest />
+			{tests?.map((test: any) => {
+				return <TypeOfTest title={test.title} />;
+			})}
 		</main>
 	);
 };
--- src/components/TypeOfTest/TypeOfTest.tsx
+++ src/components/TypeOfTest/TypeOfTest.tsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TypeOfTest.module.scss';
 import arrow from '../../static/arrow.svg';
+import classNames from 'classnames/bind';
 
-export const TypeOfTest = () => {
+const cn = classNames.bind(styles);
+
+export const TypeOfTest = (test: any) => {
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	const handleClick = () => {
+		setIsVisible((isVisible) => !isVisible);
+		console.log(isVisible);
+	};
+
 	return (
 		<>
-			<article>
+			<article
+				className={cn(styles.test__block, {
+					text__block_visible: isVisible,
+				})}>
 				<div className={styles.test__card}>
-					<div className={styles.test__card_arrow}>
+					<div className={styles.test__card_arrow} onClick={handleClick}>
 						<img src={arrow} alt='' />
 					</div>
-					<h6 className={styles.test__card_title}>Электрофизические измерения (ЭФИ)</h6>
+					<h6 className={styles.test__card_title}>{test.title}</h6>
 				</div>
 
-				<div className={styles.test__card_description}>
-					<div className={styles.test__card_line}></div>
+				<div
+					className={cn(styles.test__card_description, {
+						test__card_description_visible: isVisible,
+					})}>
+					<div
+						className={cn(styles.test__card_line, {
+							test__card_line_visible: isVisible,
+						})}></div>
 					<article>
-						<p className={styles.test__card_text}>
+						<p
+							className={cn(styles.test__card_text, {
+								test__card_text_visible: isVisible,
+							})}>
 							Измерение сопротивления изоляции линий и аппаратов, измерение
 							сопротивления заземляющих устройств (ЗУ), измерение сопротивления
 							молниезащиты, проверка наличия цепи заземления, проверка
 							цепи«фаза-нуль», проверка действия расцепителей автоматических
 							выключателей (прогрузка)
 						</p>
-						<div className={styles.test__card_block}>
+						<div
+							className={cn(styles.test__card_block, {
+								test__card_block_visible: isVisible,
+							})}>
 							<a href='#' className={styles.test__card_link}>
 								Подробнее
 							</a>
